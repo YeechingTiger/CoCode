1f6406ac805c354b325f602997162e6814265ff3
JasonHo <hexing_h@hotmail.com>
2017-09-04 00:24:19 -0400

    Complete coding problem part.
    Complete coding problem part using Angular and Angular Material.

--- frontEnd/ojClient/src/app/app.module.ts
+++ frontEnd/ojClient/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import {MdButtonModule, MdCardModule} from '@angular/material';
+
+import { routing } from './app.routes';
+
+import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCardModule,
+    routing
   ],
-  providers: [],
+  providers: [{
+    provide: "data",
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
