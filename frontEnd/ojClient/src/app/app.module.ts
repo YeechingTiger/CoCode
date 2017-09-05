c6461c379d2dd21b5921bbfcaf3a5ffcaa30fff0
JasonHo <hexing_h@hotmail.com>
2017-09-05 00:34:32 -0400

    Complete Nav Bar FrontEnd part.

--- frontEnd/ojClient/src/app/app.module.ts
+++ frontEnd/ojClient/src/app/app.module.ts
@@ -4,22 +4,30 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import {MdButtonModule, MdCardModule} from '@angular/material';
-
+import {MdMenuModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdIconModule} from '@angular/material';
 import { routing } from './app.routes';
 
 import { DataService } from './services/data.service';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdButtonModule, MdCardModule,
+    MdButtonModule, 
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
     routing
   ],
   providers: [{
